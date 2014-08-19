01b8e9228c526e0ec8679eefd21a64ee538f19a3
Anthony Kirwan <amkirwan@gmail.com>
2014-08-19 00:20:24 -0500

    reopen no longer needed

--- tests/dummy/app/initializers/ember_oauth2.js
+++ tests/dummy/app/initializers/ember_oauth2.js
@@ -4,19 +4,11 @@ import {config, hashConfig, savedStateConfig, reopenConfig} from 'ember-token-au
 export default {
   name: 'ember-oauth2-config',
 
-  initialize: function(container/*, app*/) {
+  initialize: function(/*container, app*/) {
     config();
-    var hash = hashConfig();
+    hashConfig();
     savedStateConfig();
     var c = reopenConfig();
     Ember.OAuth2.reopen(c);
-
-    Ember.OAuth2.reopen({
-      // need to set session for acceptance tests
-      openWindow: function() {
-        var session = container.lookup('session:current');
-        session.auth.trigger('redirect', hash); 
-      }
-    });
   }
 };
