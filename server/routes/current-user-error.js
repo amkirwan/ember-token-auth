4b7f5d8c1c690d7849e90244c1dbe175d179026b
Anthony Kirwan <amkirwan@gmail.com>
2014-10-20 14:21:29 -0500

    fix deprecation warning send method to staus(status).send(body)

--- server/routes/current-user-error.js
+++ server/routes/current-user-error.js
@@ -3,7 +3,7 @@ var express = require('express');
 module.exports = function(app) {
   var currentUserRouter = express.Router();
   currentUserRouter.get('/', function(req, res) {
-    res.send(500, { error: 'login error' });
+    res.status(500).send({ error: 'login error' });
   });
   app.use('/api/current_user_error', currentUserRouter);
 };
--- server/routes/current-user.js
+++ server/routes/current-user.js
@@ -3,7 +3,7 @@ var express = require('express');
 module.exports = function(app) {
   var currentUserRouter = express.Router();
   currentUserRouter.get('/', function(req, res) {
-    res.send(200, {user:{'id':'1', 'firstname': 'foo', 'lastname': 'bar'}});
+    res.status(200).send({user:{'id':'1', 'firstname': 'foo', 'lastname': 'bar'}});
   });
   app.use('/api/current_user', currentUserRouter);
 };
