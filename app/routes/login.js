58dc652b99c759cbb89492cb983db5096b3efb14
Anthony Kirwan <amkirwan@gmail.com>
2014-10-23 22:44:24 -0500

    do not redirect from login route if session is not expired
    should be left to handle on a per app

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function(/*transition*/) {
-    if (this.session.isNotExpired()) {
-      this.transitionTo('index');
-    } 
-  },
 
   model: function() {
     if (this.get('provider')) {
