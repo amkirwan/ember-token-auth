e78a8e24f9cebb656ce221298ba72e85b4aa9411
Anthony Kirwan <amkirwan@gmail.com>
2014-12-18 17:34:19 -0600

    update 500 error response

--- server/mocks/current-user-error.js
+++ server/mocks/current-user-error.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   var express = require('express');
   var currentUserRouter = express.Router();
   currentUserRouter.get('/', function(req, res) {
-    res.status(500).send({ error: 'login error' });
+    res.status(500).send({error:{title: 'internal_service_error', detail: 'internal_service_error', status: '500'}});
   });
   app.use('/api/current_user_error', currentUserRouter);
 };
