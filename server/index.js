a42b78d7cebea4877cf43f9c742f56ad83c8aeea
Anthony Kirwan <amkirwan@gmail.com>
2015-07-21 15:13:53 -0500

    update mock-server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
+// To use it create some files under `mocks/`
+// e.g. `server/mocks/ember-hamsters.js`
+//
+// module.exports = function(app) {
+//   app.get('/ember-hamsters', function(req, res) {
+//     res.send('hello');
+//   });
+// };
+
 module.exports = function(app) {
   var globSync   = require('glob').sync;
   var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
-  var bodyParser = require('body-parser');
-  app.use(bodyParser.json());
+  // Log proxy requests
+  var morgan  = require('morgan');
+  app.use(morgan('dev'));
 
-
-  // proxy expects a stream, but express will have turned
-  // the request stream into an object because bodyParser
-  // has run. We have to convert it back to stream:
-  // https://github.com/nodejitsu/node-http-proxy/issues/180
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
+
 };
--- server/mocks/current-user.js
+++ server/mocks/current-user.js
@@ -2,7 +2,6 @@ module.exports = function(app) {
   var express = require('express');
   var currentUserRouter = express.Router();
   currentUserRouter.get('/', function(req, res) {
-    // res.status(200).send({user:{'id':'1', 'firstname': 'foo', 'lastname': 'bar'}});
     res.status(200).send({"data":{"type":"users","id":"1","attributes":{"firstname":"foo","lastname":"bar"}}});
   });
   app.use('/api/current_user', currentUserRouter);
