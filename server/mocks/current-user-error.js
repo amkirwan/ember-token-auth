6c595a9ea71694430cb77cf343f959e070b90916
Anthony Kirwan <amkirwan@gmail.com>
2015-07-21 15:14:18 -0500

    update error response to match json-api 1.0

--- server/mocks/current-user-error.js
+++ server/mocks/current-user-error.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   var express = require('express');
   var currentUserRouter = express.Router();
   currentUserRouter.get('/', function(req, res) {
-    res.status(500).send({error:{title: 'internal_service_error', detail: 'internal_service_error', status: '500'}});
+    res.status(500).send({"errors":[{"id":"1","status":"500","title":"internal_service_error","detail":"internal_service_error_detail"}]})
   });
   app.use('/api/current_user_error', currentUserRouter);
 };
