a67696f7d6a516c875af448aabfbfb9987e1c7f1
Anthony Kirwan <amkirwan@gmail.com>
2015-07-21 23:27:44 -0500

    ember init update

---
+++ ember-cli-build.js
@@ -0,0 +1,28 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+
+  });
+
+  app.import('bower_components/ember-oauth2/dist/ember-oauth2.amd.js', {
+    exports: {
+      'ember-oauth2': [ 'default' ]
+    }
+  });
+
+  if (app.env === 'development' || app.env === 'test') {
+    app.import('bower_components/sinon-browser-only/sinon.js');
+  }
+
+  /*
+    This build file specifes the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  return app.toTree();
+};
