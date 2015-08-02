6e71210ef2ab1f725e13fcdc376a0549813ace58
Anthony Kirwan <amkirwan@gmail.com>
2015-08-02 00:05:39 -0500

    observe loginError change on didInsertElement

--- tests/dummy/app/components/current-session.js
+++ tests/dummy/app/components/current-session.js
@@ -2,14 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['current-user'],
+  loginError: false,
 
-  logginError: function() {
-    // if the loggin error is true update the view
-    if (this.get('logginError')) {
+  didInsertElement: function() {
+    Ember.addObserver(this, 'loginError', this, this.loginErrorChanged);
+  },
+
+  loginErrorChanged: function(/*comp, value*/) {
+    if (this.get('loginError')) {
       Ember.run.once(this, function() {
         Ember.$('.current-user').html('<p class="error">There was an error logging in. Please try again.</p>');
       });
     }
-  }.observes('loginError').on('init')
+  }
 
 });
