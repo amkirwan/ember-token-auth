001b69b23d8757d6518d62e9da617424dd249f45
Anthony Kirwan <amkirwan@gmail.com>
2015-08-06 10:25:49 -0500

    should build git for git task not npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,9 @@ module.exports = function(grunt) {
   grunt.registerTask('release:patch', ['versioner:bumpOnly:default:patch', 'versioner:commitOnly:default']);
   grunt.registerTask('release:minor', ['versioner:bumpOnly:default:minor', 'versioner:commitOnly:default']);
   grunt.registerTask('release:major', ['versioner:bumpOnly:default:major', 'versioner:commitOnly:default']);
-  grunt.registerTask('latest-build:git', ['versioner:bumpOnly:default:git']);
+  grunt.registerTask('latest-build:git', ['versioner:bumpOnly:git']);
 
-  grunt.registerTask('release:npm', ['versioner:bumpOnly:git']);
+  grunt.registerTask('release:npm', ['versioner:npmOnly:default']);
 
   grunt.initConfig(config);
 };
