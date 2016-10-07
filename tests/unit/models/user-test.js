22ce5f91ea0835baefe4bd2ac7df6ffebacf8f08
Anthony Kirwan <amkirwan@gmail.com>
2016-10-07 01:01:15 -0400

    update qunit description

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -1,6 +1,6 @@
 import { test, moduleForModel } from 'ember-qunit';
 
-moduleForModel('user', 'User', {
+moduleForModel('user', 'Unit | User | user', {
   // Specify the other units that are required for this test.
   needs: []
 });
